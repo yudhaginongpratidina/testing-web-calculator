bdf1598f5103004ccc6c524040174eea810fa64d
yudhaginongpratidina <yudhaginong071102@gmail.com>
2023-10-28 18:44:44 +0700

    Setup Environment for Testing

---
+++ tests/calculator.test.js
@@ -0,0 +1,10 @@
+import Calculator from "../public/calculator"
+
+// TEST CASE
+// 1. Dapat melakukan operasi aritmatika dengan baik
+// 2. Mengembalikan error ketika memasukkan nilai string
+// 3. Mengembalika error ketika memasukkan nilai NaN
+
+describe('Calculator', () => {
+
+})
