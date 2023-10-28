66b84828a0649a5f94fd90673bfec5f7074c8a83
yudhaginongpratidina <yudhaginong071102@gmail.com>
2023-10-28 19:26:57 +0700

    Test Case 2 : Mengembalikan error ketika memasukkan nilai string

--- public/calculator.js
+++ public/calculator.js
@@ -1,16 +1,31 @@
 const Calculator = {
     penjumlahan(firstNumber, secondNumber) {
+        if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+            throw new Error('Parameter firstNumber dan secondNumber harus berupa angka!');
+        }
         return firstNumber + secondNumber;
     },
     pengurangan(firstNumber, secondNumber) {
+        if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+            throw new Error('Parameter firstNumber dan secondNumber harus berupa angka!');
+        }
+        
         return firstNumber - secondNumber;
     },
     perkalian(firstNumber, secondNumber) {
+        if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+            throw new Error('Parameter firstNumber dan secondNumber harus berupa angka!');
+        }
+
         return firstNumber * secondNumber;
     },
     pembagian(firstNumber, secondNumber) {
+        if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+            throw new Error('Parameter firstNumber dan secondNumber harus berupa angka!');
+        }
+
         return firstNumber / secondNumber;
     }
-}
+};
 
 export default Calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,31 +1,38 @@
-import Calculator from "../public/calculator"
-
-// TEST CASE
-// 1. Dapat melakukan operasi aritmatika dengan baik
-// 2. Mengembalikan error ketika memasukkan nilai string
-// 3. Mengembalika error ketika memasukkan nilai NaN
+import Calculator from "../public/calculator";
 
 describe('Calculator', () => {
     const firstNumber = 10;
     const secondNumber = 5;
 
-    it ('Operasi Penjumlahan', () => {
+    const firstString = '10';
+    const secondString = '5';
+
+    it('Operasi Penjumlahan', () => {
         const result = Calculator.penjumlahan(firstNumber, secondNumber);
         expect(result).toBe(15);
     });
 
-    it ('Operasi Pengurangan', () => {
-       const result = Calculator.pengurangan(firstNumber, secondNumber);
-       expect(result).toBe(5); 
+    it('Operasi Pengurangan', () => {
+        const result = Calculator.pengurangan(firstNumber, secondNumber);
+        expect(result).toBe(5);
     });
 
-    it ('Operasi Perkalian', () => {
+    it('Operasi Perkalian', () => {
         const result = Calculator.perkalian(firstNumber, secondNumber);
         expect(result).toBe(50);
     });
 
-    it ('Operasi Pembagian', () => {
+    it('Operasi Pembagian', () => {
         const result = Calculator.pembagian(firstNumber, secondNumber);
         expect(result).toBe(2);
     });
-})
+
+    it('Mengembalikan error ketika memasukkan nilai string', () => {
+        expect(() => Calculator.penjumlahan(firstString, secondString)).toThrow();
+        expect(() => Calculator.pengurangan(firstString, secondString)).toThrow();
+        expect(() => Calculator.perkalian(firstString, secondString)).toThrow();
+        expect(() => Calculator.pembagian(firstString, secondString)).toThrow();
+    });
+});
+
+
