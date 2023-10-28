21b49d5543825c5faf267273ef1ea5ceb7c7069c
yudhaginongpratidina <yudhaginong071102@gmail.com>
2023-10-28 19:04:05 +0700

    Test Case 1 : Dapate melakukan operasi aritmatika dengan baik

--- public/calculator.js
+++ public/calculator.js
@@ -1,5 +1,16 @@
 const Calculator = {
-
+    penjumlahan(firstNumber, secondNumber) {
+        return firstNumber + secondNumber;
+    },
+    pengurangan(firstNumber, secondNumber) {
+        return firstNumber - secondNumber;
+    },
+    perkalian(firstNumber, secondNumber) {
+        return firstNumber * secondNumber;
+    },
+    pembagian(firstNumber, secondNumber) {
+        return firstNumber / secondNumber;
+    }
 }
 
 export default Calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -6,5 +6,26 @@ import Calculator from "../public/calculator"
 // 3. Mengembalika error ketika memasukkan nilai NaN
 
 describe('Calculator', () => {
+    const firstNumber = 10;
+    const secondNumber = 5;
 
+    it ('Operasi Penjumlahan', () => {
+        const result = Calculator.penjumlahan(firstNumber, secondNumber);
+        expect(result).toBe(15);
+    });
+
+    it ('Operasi Pengurangan', () => {
+       const result = Calculator.pengurangan(firstNumber, secondNumber);
+       expect(result).toBe(5); 
+    });
+
+    it ('Operasi Perkalian', () => {
+        const result = Calculator.perkalian(firstNumber, secondNumber);
+        expect(result).toBe(50);
+    });
+
+    it ('Operasi Pembagian', () => {
+        const result = Calculator.pembagian(firstNumber, secondNumber);
+        expect(result).toBe(2);
+    });
 })
